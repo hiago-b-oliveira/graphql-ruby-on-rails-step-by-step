d0bd360f56f93e7207876db552dd306e0eda1ea3
Hiago Borges <hiago.borges@olxbr.com>
2021-05-16 14:36:33 -0300

    add the create author mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -6,5 +6,20 @@ module Types
     def test_field
       "Hello World"
     end
+
+    # Inline mutation
+    # field :create_author, AuthorType, null: true, description: "Create an author" do
+    #   argument :first_name, String, required: false
+    #   argument :last_name, String, required: false
+    #   argument :yob, Int, required: false
+    #   argument :is_alive, Boolean, required: false
+    # end
+    #
+    # def create_author(first_name:, last_name:, yob:, is_alive:)
+    #   Author.create first_name: first_name, last_name: last_name, yob: yob, is_alive: is_alive
+    # end
+
+    # Separated file mutation
+    field :create_author, Types::AuthorType, mutation: Mutations::CreateAuthor
   end
 end
